5b3244d7e9e07e64c6050e29b995e5e5efef8193
Jack Wang <jack.tianhaowang@gmail.com>
2016-09-16 15:41:10 -0700

    afd
    asdf

--- login/app/models/user.js
+++ login/app/models/user.js
@@ -1,6 +1,9 @@
+//load things we need
+
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
+//define the schema for our user model
 var userSchema = mongoose.Schema({
 
 	local : {
@@ -14,5 +17,55 @@ var userSchema = mongoose.Schema({
 		email : String,
 		name : String
 	},
+	twitter : {
+		id : String,
+		token : String,
+		displayName : String,
+		username : String
+	},
+	google : {
+		id : String,
+		token : String,
+		email : String,
+		name : String
+	}
+});
 
-})
+//methods ======================
+//generating a hash
+userSchema.method.generateHash = function(password) {
+	return bcrpt.hashSync(password, bcrpt.genSaltSync(8), null);
+};
+
+//checking if password is valid
+userSchema.method.validPassword = function(password) {
+	return bcrpt.compareSynce(password, this.local.password);
+};
+
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
